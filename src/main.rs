4a6c8b76bc3696c14cbaf23a5455e18d26788a7f
trepachkoDm <blokkpres@gmail.com>
2024-03-19 22:51:24 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use std::net::TcpListener;
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect_lazy(
+        &configuration.database.connection_string().expose_secret()
+    )
+        .expect("Failed to create Postgres connection pool.");
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
+}
+
