a1e8c10dca6e75b542db7650c142d8c4da52f397
trepachkoDm <blokkpres@gmail.com>
2024-03-20 21:48:18 +0300

    chapter_6_15

--- src/domain/subscriber_name.rs
+++ src/domain/subscriber_name.rs
@@ -38,11 +38,6 @@ impl AsRef<str> for SubscriberName {
     }
 }
 
-pub struct NewSubscriber {
-    pub email: String,
-    pub name: SubscriberName,
-}
-
 #[cfg(test)]
 mod tests {
     use claims::{assert_err, assert_ok};
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,12 +3,28 @@ use chrono::Utc;
 use sqlx::{ PgPool};
 use uuid::Uuid;
 use unicode_segmentation::UnicodeSegmentation;
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
    pub email: String,
     pub name: String
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
+}
+
+pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form.name)?;
+    let email = SubscriberEmail::parse(form.email)?;
+    Ok(NewSubscriber { email, name })
 }
 
 #[tracing::instrument(
@@ -24,16 +40,31 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
-        // Return early if the name is invalid, with a 400
+
+    // let name = match SubscriberName::parse(form.0.name) {
+    //     Ok(name) => name,
+    //     Err(_) => return HttpResponse::BadRequest().finish(),
+    // };
+    //
+    // let email = match SubscriberEmail::parse(form.0.email) {
+    //     Ok(email) => email,
+    //     Err(_) => return HttpResponse::BadRequest().finish(),
+    // };
+
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
 
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name
-    };
+    // let new_subscriber = match parse_subscriber(form.0) {
+    //     Ok(subscriber) => subscriber,
+    //     Err(_) => return HttpResponse::BadRequest().finish(),
+    // };
+
+    // let new_subscriber = NewSubscriber {
+    //     email,
+    //     name
+    // };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -66,7 +97,7 @@ pub async fn insert_subscriber(
     VALUES ($1, $2, $3, $4)
             "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
