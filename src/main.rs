9bf0f9d3fa8423d922fc277f3e38adbea3235861
trepachkoDm <blokkpres@gmail.com>
2024-03-23 20:38:15 +0300

    ch7

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use zero2prod::startup::run;
 use std::net::TcpListener;
 use sqlx::postgres::PgPoolOptions;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use zero2prod::email_client::EmailClient;
 
 
 
@@ -15,8 +16,20 @@ async fn main() -> std::io::Result<()> {
 
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        // `connect_lazy_with` instead of `connect_lazy`
         .connect_lazy_with(configuration.database.with_db());
+
+    // Build an `EmailClient` using `configuration`
+    let sender_email = configuration.email_client.sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout
+    );
 
     let address = format!(
         "{}:{}",
@@ -24,7 +37,7 @@ async fn main() -> std::io::Result<()> {
     );
 
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await?;
+    run(listener, connection_pool, email_client)?.await?;
     Ok(())
 }
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,6 +5,7 @@ use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
+use zero2prod::email_client::EmailClient;
 
 
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -36,7 +37,21 @@ async fn spawn_app() -> TestApp {
     configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
+    // Build a new email client
+    let sender_email = configuration.email_client.sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout
+    );
+
+    let server = run(listener, connection_pool.clone(), email_client).expect("Failed to bind address");
+
     let _ = tokio::spawn(server);
     TestApp {
         address,
